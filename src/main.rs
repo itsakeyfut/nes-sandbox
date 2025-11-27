f1c083d803484a1c3a3b92f0dc67346dad6be1bd
itsakeyfut <sicsempertyrannis98@gmail.com>
2025-11-27 14:21:01 +0900

    refactor: capsulize lda and tax

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,30 @@ impl CPU {
         }
     }
 
+    fn lda(&mut self, value: u8) {
+        self.register_a = value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self) {
+        self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        if result == 0 {
+            self.status |= 0b0000_0010; // Set zero flag
+        } else {
+            self.status &= 0b1111_1101; // Clear zero flag
+        }
+
+        if result & 0b1000_0000 != 0 {
+            self.status |= 0b1000_0000; // Set negative flag
+        } else {
+            self.status &= 0b0111_1111; // Clear negative flag
+        }
+    }
+
     // A very simple interpreter for a subset of 6502 instructions
     pub fn interpret(&mut self, program: Vec<u8>) {
         self.program_counter = 0;
@@ -32,35 +56,10 @@ impl CPU {
                 0xA9 => { // LDA - Load Accumulator Immediate
                     let param = program[self.program_counter as usize];
                     self.program_counter += 1;
-                    self.register_a = param;
-
-                    if self.register_a == 0 {
-                        self.status |= 0b0000_0010; // Set zero flag
-                    } else {
-                        self.status &= 0b1111_1101; // Clear zero flag
-                    }
-
-                    if self.register_a & 0b1000_0000 != 0 {
-                        self.status |= 0b1000_0000; // Set negative flag
-                    } else {
-                        self.status &= 0b0111_1111; // Clear negative flag
-                    }
-                }
-                0xAA => { // TAX - Transfer Accumulator to X
-                    self.register_x = self.register_a;
                     
-                    if self.register_x == 0 {
-                        self.status |= 0b0000_0010; // Set zero flag
-                    } else {
-                        self.status &= 0b1111_1101; // Clear zero flag
-                    }
-
-                    if self.register_x & 0b1000_0000 != 0 {
-                        self.status |= 0b1000_0000; // Set negative flag
-                    } else {
-                        self.status &= 0b0111_1111; // Clear negative flag
-                    }
+                    self.lda(param);
                 }
+                0xAA => self.tax(), // TAX - Transfer Accumulator to X
                 0x00 => return, // BRK - Break
                 _ => todo!("Implement opscode: {opscode:#X}"),
             }
@@ -100,6 +99,6 @@ mod tests {
         let mut cpu = CPU::new();
         cpu.register_a = 10;
         cpu.interpret(vec![0xaa, 0x00]); // TAX; BRK
-        assert_eq!(cpu.register_x, 10)
+        assert_eq!(cpu.register_x, 10);
     }
 }
